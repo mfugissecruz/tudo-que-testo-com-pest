01805ec7c7011d953bc9a3dc4bcce70f9fd32cc3
marcelocruz <mfugissecruz@gmail.com>
2024-02-28 22:14:58 -0300

    testing page response json

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('products', function () {
+    $products = Product::all();
+    $products->map(fn($product) => ['title' => $product->title]);
+
+    return array_merge([
+        ['title' => 'Product A'],
+        ['title' => 'Product B'],
+        ['title' => 'Product C'],
+    ], $products->toArray());
 });
---
+++ tests/Feature/PageResponseJsonTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Product;
+
+use function Pest\Laravel\get;
+
+test('nossa api de produtos precisa retornar a lista de produtos')
+    ->get('/api/products')
+    ->assertOk()
+    ->assertExactJson([
+        ['title' => 'Product A'],
+        ['title' => 'Product B'],
+        ['title' => 'Product C'],
+    ]);
+
+test('must list products from database', function () {
+    $product1 = Product::factory()->create();
+    $product2 = Product::factory()->create();
+
+    get('/api/products')
+        ->assertOk()
+        ->assertJson([
+            ['title' => 'Product A'],
+            ['title' => 'Product B'],
+            ['title' => 'Product C'],
+            ['title' => $product1->title],
+            ['title' => $product2->title]
+        ]);
+});
