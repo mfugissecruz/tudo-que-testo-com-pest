376bb814fb058b138dbb47d4c6250d70331f2be1
marcelocruz <mfugissecruz@gmail.com>
2024-02-28 21:03:27 -0300

    starting with pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,5 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
+test('returns a successful response')
+    ->get('/')
+    ->assertSuccessful();
