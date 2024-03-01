10483aadf9503d39b5fcbc01a1c9708076bb8476
marcelocruz <mfugissecruz@gmail.com>
2024-02-29 23:06:59 -0300

    testing model relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title'];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
